4b2bfcbb128f445a0dd5230e035dcb087bbc807d
NotSeriousCoder <710267819@qq.com>
2019-04-02 20:48:24 +0800

    Jack change file

--- MainActivity.java
+++ MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
 //                }
 //            }
 //        };
-//        thread.sstart();
+//        thread.stop();
 //        thread.join();
 //        Log.d("count == " + SyncTest.count);
     }
